b4ed80a4d4e5a7dc4afa8a33b904064f278b6c28
Kuha <khalil.stessens@ing.austral.edu.ar>
2021-03-26 10:00:13 -0300

    Made a switch

--- lexer/src/main/java/lexer/PSLexer.java
+++ lexer/src/main/java/lexer/PSLexer.java
@@ -5,6 +5,7 @@ import token.Token;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class PSLexer  implements Lexer{
     @Override
@@ -15,7 +16,7 @@ public class PSLexer  implements Lexer{
         for (int i = 0; i <text.size() ; i++) {
             //line to char
             List<String> currentLine = lineToChars(text.get(i));
-            lineToToken(currentLine);
+            lineToToken(currentLine,i);
 
         }
 
@@ -28,18 +29,29 @@ public class PSLexer  implements Lexer{
         return chars;
     }
 
-    private List<Token> lineToToken(List<String> line){
+    private List<Token> lineToToken(List<String> line, int lineNumber){
         String currentWord = "";
+        List<Token> list = new ArrayList<Token>();
         for (int i = 0; i <line.size() ; i++) {
-            System.out.println(line.get(i));
-        }
-        for (int i = 0; i <line.size() ; i++) {
-            if (line.get(i).equals(" ")){
-                System.out.println(currentWord);
-                currentWord="";
+            Optional<Token> token = tokenIdentifier(currentWord,lineNumber,i);
+            if(token.isPresent()){
+                list.add(token.get());
+                currentWord = "";
             }
             else currentWord+=line.get(i);
         }
-        return new ArrayList<Token>();
+        return list;
+    }
+
+    private Optional<Token> tokenIdentifier(String token, int lineNumber, int columnNumber){
+        return switch (token) {
+            case "let" -> Optional.of(Token.let(lineNumber, columnNumber));
+            case "string" -> Optional.of(Token.string(lineNumber, columnNumber));
+            case "number" -> Optional.of(Token.integer(lineNumber, columnNumber));
+            case "=" -> Optional.of(Token.assignation(lineNumber, columnNumber));
+            case ":" -> Optional.of(Token.colon(lineNumber, columnNumber));
+            case ";" -> Optional.of(Token.semicolon(lineNumber, columnNumber));
+            default -> Optional.empty();
+        };
     }
 }
--- tokens/src/main/java/token/Token.java
+++ tokens/src/main/java/token/Token.java
@@ -8,6 +8,46 @@ public class Token {
     int startingLine;
     int endingLine;
 
+    private Token(TokenType type, String value, int startingColumn, int endingColumn, int startingLine, int endingLine) {
+        this.type = type;
+        this.value = value;
+        this.startingColumn = startingColumn;
+        this.endingColumn = endingColumn;
+        this.startingLine = startingLine;
+        this.endingLine = endingLine;
+    }
+
+    public static Token let(int ln, int cn){
+        return new Token(TokenType.LET,"let",cn,cn+2,ln,ln);
+    }
+
+    public static Token string(int ln, int cn){
+        return new Token(TokenType.STRING,"string",cn,cn+5,ln,ln);
+    }
+
+    public static Token integer(int ln, int cn) {
+        return new Token(TokenType.INTEGER,"integer",cn,cn+6,ln,ln);
+    }
+
+    public static Token assignation(int ln, int cn){
+        return new Token(TokenType.ASSIGNATION,"=",cn,cn,ln,ln);
+    }
+
+    public static Token colon(int ln, int cn){
+        return new Token(TokenType.COLON,":",cn,cn,ln,ln);
+    }
+
+    public static Token semicolon(int ln, int cn){
+        return new Token(TokenType.SEMICOLON,";",cn,cn,ln,ln);
+    }
+
+    public static Token identifier(String value, int ln, int cn){
+        return new Token(TokenType.IDENTIFIER,value,cn,cn+value.length()-1,cn,cn);
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
 
 enum TokenType {
@@ -15,6 +55,7 @@ enum TokenType {
     INTEGER,
     ASSIGNATION,
     IDENTIFIER,
+    COLON,
     SEMICOLON,
     LET,
     ADDITION,
